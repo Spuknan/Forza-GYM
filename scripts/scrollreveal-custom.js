ac70991eab535436af3c491c1a8f814fe284eada
Hernán Rojas <horojas96@gmail.com>
2022-09-22 14:51:19 -0300

    nosotros.html styled. Still have to link properly

--- scripts/scrollreveal-custom.js
+++ scripts/scrollreveal-custom.js
@@ -6,6 +6,24 @@ sr.reveal('.scroll-updown', {
     distance: '-100px',
 })
 
-sr.reveal('.scroll-fade', {
+sr.reveal('.scroll-downup', {
     duration: 2000,
+    origin: 'top',
+    distance: '-200px',
+})
+
+sr.reveal('.scroll-fade', {
+    duration: 3000,
+})
+
+sr.reveal('.scroll-leftright', {
+    duration: 2000,
+    origin: 'right',
+    distance: '-100px',
+})
+
+sr.reveal('.scroll-rightleft', {
+    duration: 2000,
+    origin: 'left',
+    distance: '-100px',
 })
